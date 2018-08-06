62539c4b6d88ebc9963c9d18a404ecbda05f029f
vitorleo80 <vitorleo@hotmail.com>
2018-08-06 08:16:17 +0100

    edit setup

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import FileSaver from 'file-saver'
 
 class App extends Component {
   state = {
-    notes: []
+    notes: [],
+    editMode: false,
+    note: {}
   }
 
   componentDidMount() {
@@ -21,10 +23,11 @@ class App extends Component {
   }
 
   render() {
+    const { editMode, note } = this.state
     return (
       <div className="container">
         <h1>Note Taking App</h1>
-        <Note onAddNote={this.addNotetoState}/>
+        <Note onAddNote={this.addNotetoState} isEditMode={editMode} note={note} />
         <h4>Notes Count: <span className='badge'>{this.state.notes.length}</span></h4>
 
         <ul className='list-group'>
@@ -37,9 +40,12 @@ class App extends Component {
                 className='bnt btn-danger btn-sm'><span className='glyphicon glyphicon-trash'></span>Delete</button>
               {" "}
               <button
+                onClick={() => this.editNote(note.id)}
+                className='btn btn-primary btn-sm'><span className='glyphicon glyphicon-file'></span>Edit</button>
+              {" "}
+              <button
                 onClick={() => this.exportNote(i)}
                 className='btn btn-warning btn-sm'><span className='glyphicon glyphicon-file'></span>Export</button>
-
             </li>
           )}
         </ul>
@@ -48,12 +54,23 @@ class App extends Component {
   }
 
   addNotetoState = (note) => {
+    if (this.state.editMode) {
+      this.setState({
+        editMode: false,
+        notes: this.state.notes.map((existingNote) => {
+          if (existingNote.id === note.id) {
+            return note
+          }
+          return existingNote
+        })
+      })
+    } else {
       this.setState({
         editMode: false,
         notes: [...this.state.notes, note]
       })
     }
-  
+  }
   deleteNote = (i) => {
     this.setState({
       notes: this.state.notes.filter((note, index) => {
@@ -69,8 +86,11 @@ class App extends Component {
     const blob = new Blob([JSON.stringify(note)], { type: "text/plain;charset=utf-8" });
     FileSaver.saveAs(blob, "note.txt")
   }
-  
- 
+  editNote = (noteId) => {
+    const note = this.state.notes.find(note => note.id === noteId)
+    this.setState({ editMode: true, note })
+  }
+
 }
 
 export default App;
--- src/components/Note.js
+++ src/components/Note.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
+import uuid from 'uuid/v4'
 
 
-export default class AddNote extends Component {
+export default class Note extends Component {
     state = {
         noteTitle: '',
         noteDesc: '',
@@ -10,22 +11,19 @@ export default class AddNote extends Component {
 
     
 
-    render() {
-        return (
-            <div>
-                <form className='form-horizontal'
-                    onSubmit={this.handleSubmit}>
-                    <div className='form-group'>
-                        <label htmlFor='inputNoteTitle' className='col-sm-2 control-label'>New Note</label>
-                        <div className='col-sm-10'>
-                            <input name='noteTitle'
-                                type='text'
-                                className='form-control'
-                                id='inputNoteTitle'
-                                value={this.state.noteTitle}
-                                onChange={this.onChange}
-                                placeholder='Title' />
-                        </div>
+    renderForm = () => {
+        if (this.props.isEditMode) {
+            return (
+                <form className='form-horizontal' onSubmit={this.handleSubmit}>
+                    <label htmlFor='inputNoteTitle' className='col-sm-2 control-label'>New Note</label>
+                    <div className='col-sm-10'>
+                        <input name='noteTitle'
+                            type='text'
+                            className='form-control'
+                            id='inputNoteTitle'
+                            defaultValue={this.props.note.noteTitle}
+                            onChange={this.onChange}
+                            placeholder='Title' />
                     </div>
 
                     <div className='form-group'>
@@ -35,7 +33,7 @@ export default class AddNote extends Component {
                                 className='form-control'
                                 rows='3'
                                 id='inputNoteDesc'
-                                value={this.state.noteDesc}
+                                defaultValue={this.props.note.noteDesc}
                                 onChange={this.onChange}>
                             </textarea>
                         </div>
@@ -47,7 +45,7 @@ export default class AddNote extends Component {
                             <select name='notePriority'
                                 className='form-control'
                                 id='notePriority'
-                                value={this.state.notePriority}
+                                defaultValue={this.props.note.notePriority}
                                 onChange={this.onChange}>
                                 <option>Low</option>
                                 <option>Medium</option>
@@ -58,22 +56,100 @@ export default class AddNote extends Component {
 
                     <div className='form-group'>
                         <div className='col-sm-offset-2 col-sm-10'>
-                            <button type='submit' className='btn btn-success'>Add Note</button>
+                            <button type='submit' className='btn btn-primary'>Save Note</button>
                         </div>
                     </div>
-
                 </form>
+            )
+        }
+        return (
+            <form className='form-horizontal'
+                onSubmit={this.handleSubmit}>
+                <div className='form-group'>
+                    <label htmlFor='inputNoteTitle' className='col-sm-2 control-label'>New Note</label>
+                    <div className='col-sm-10'>
+                        <input name='noteTitle'
+                            type='text'
+                            className='form-control'
+                            id='inputNoteTitle'
+                            value={this.state.noteTitle}
+                            onChange={this.onChange}
+                            placeholder='Title' />
+                    </div>
+                </div>
+
+                <div className='form-group'>
+                    <label htmlFor='inputNoteDesc' className='col-sm-2 control-label'>Description</label>
+                    <div className='col-sm-10'>
+                        <textarea name='noteDesc'
+                            className='form-control'
+                            rows='3'
+                            id='inputNoteDesc'
+                            value={this.state.noteDesc}
+                            onChange={this.onChange}>
+                        </textarea>
+                    </div>
+                </div>
+
+                <div className='form-group'>
+                    <label htmlFor='inputNotePriority' className='col-sm-2 control-label'>Priority</label>
+                    <div className='col-sm-10'>
+                        <select name='notePriority'
+                            className='form-control'
+                            id='notePriority'
+                            value={this.state.notePriority}
+                            onChange={this.onChange}>
+                            <option>Low</option>
+                            <option>Medium</option>
+                            <option>High</option>
+                        </select>
+                    </div>
+                </div>
+
+                <div className='form-group'>
+                    <div className='col-sm-offset-2 col-sm-10'>
+                        <button type='submit' className='btn btn-success'>Add Note</button>
+                    </div>
+                </div>
+
+            </form>
+        )
+    }
+
+    render() {
+        return (
+            <div>
+                {this.renderForm()}
             </div>
         )
     }
     handleSubmit = (e) => {
         e.preventDefault()
-            this.props.onAddNote(this.state)
+        if (!this.props.isEditMode) {
+            const { noteDesc, notePriority, noteTitle } = this.state
+            const note = {
+                id: uuid(),
+                noteDesc,
+                notePriority,
+                noteTitle
+            }
+            this.props.onAddNote(note)
             this.setState({
                 noteTitle: '',
                 noteDesc: '',
                 notePriority: 'Low'
             })
+        } else {
+            const { note } = this.props
+            const newNote = {
+                id: note.id,
+                noteDesc: this.state.noteDesc || note.noteDesc,
+                notePriority: this.state.notePriority || note.notePriority,
+                noteTitle: this.state.noteTitle || note.noteTitle
+            }
+            console.log(newNote)
+            this.props.onAddNote(newNote)
+        }
     }
 
     onChange = ({ target: { value, name } }) => {
